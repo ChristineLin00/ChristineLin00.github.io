de2512d477d70daa928d1fc72d74055a433d5fd2
Christine <christine.cl.lin@mail.foxconn.com>
2020-10-08 16:10:58 +0800

    modify javascript_5

--- javascript/js_5.js
+++ javascript/js_5.js
@@ -6,5 +6,10 @@ function which_max_3(a, b, c) {
   return which_max_2(a, b) > c ? which_max_2(a, b) : c;
 }
 
+function which_max_3_(a, b, c) {
+  return ((a > b && a > c)? a :(b > c)? b : c);
+}
+
 console.log(which_max_2(5, -1));
 console.log(which_max_3(5, 41, -5));
+console.log(which_max_3_(5, -5, 41));
