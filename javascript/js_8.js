a618a3146be36002a4a3a4432738e46e0867777d
Christine <christine.cl.lin@mail.foxconn.com>
2020-10-13 11:59:38 +0800

    add javascript_practice_8

---
+++ javascript/js_8.js
@@ -0,0 +1,29 @@
+let input_list = ['Dog', 'cat', 'Giraffe', 'gorilla', 'dog', 'parrot', 'doG', 'giraFFe', 'Cat'];
+// compare output and input
+function getUnique_insensitive(arr) {
+  var ouput_list = [];
+  for (i = 0; i < arr.length; i++) {
+    regex = new RegExp(arr[i], 'i');
+    if (ouput_list.findIndex((x) => regex.test(x)) === -1 && arr[i] !== "") {
+      ouput_list.push(arr[i]);
+    }
+  }
+  return ouput_list;
+}
+
+console.log(getUnique_insensitive(input_list));
+
+// delete the duplicated data
+function getUnique_insensitive2(arr) {
+  var i = 1;
+  while(arr[i]) {
+    regex = new RegExp(arr[i], 'i');
+    if (arr.slice(0, i).findIndex((x) => regex.test(x)) !== -1) {
+      arr.splice(i, 1);
+      i = i - 1;
+    }
+    i ++;
+  }
+  return arr;
+}
+console.log(getUnique_insensitive2(input_list));
